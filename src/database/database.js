9cbb2fb89c802ce7e427e2a20b4703e691aad2e8
Thanh Dong <nipponwfm@gmail.com>
2020-10-10 14:23:29 +0700

    update env

--- src/database/database.js
+++ src/database/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // module.exports = async () => {
 //     await 
-mongoose.connect('mongodb://127.0.0.1:27017/shopping-website', {
+mongoose.connect(process.env.dbURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
